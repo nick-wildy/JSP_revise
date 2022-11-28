e89ff6d29f92332819e787663b9c02edbc55ab0b
nick-wildy <117957216+nick-wildy@users.noreply.github.com>
2022-11-28 14:54:31 +0900

    Add files via upload
    day32
    1. study for built in Object (pageContext, reqeust, session, application)
    2. practice page making

---
+++ day05/src/ex02/Human.java
@@ -0,0 +1,36 @@
+package ex02;
+
+public class Human {
+	private String name;
+	private int age;
+	
+	public Human() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Human(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	
+}
